84743fb8d3b332c4aaebd07410a3a59585b3b186
nanamuramarina <nanamuramarina@gmail.com>
2023-12-28 14:42:47 +0000

    とちゅう

---
+++ NameArray.java
@@ -0,0 +1,14 @@
+public class NameArray {
+  public static void main (String[] args) {
+    String[] names = {"桜井","田原","中川","鈴木"};
+    String Seach = args[0];
+    boolean result = false;
+    
+    for (int i = 0; i< names.length; i++){
+      if (search.equals(names[i])){
+        result = true;
+      }
+      if ()
+    }
+  }
+}
