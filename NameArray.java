4342bd7933be1cdc31b3417ad2224e9c47237170
nanamuramarina <nanamuramarina@gmail.com>
2023-12-29 14:00:18 +0000

    “プログラム演習”

--- NameArray.java
+++ NameArray.java
@@ -1,14 +1,26 @@
 public class NameArray {
-  public static void main (String[] args) {
-    String[] names = {"桜井","田原","中川","鈴木"};
-    String Seach = args[0];
+  public static void main(String[] args) {
+    String[] names = {"桜井", "田原", "中川", "鈴木"};
+
+    if (args.length == 0) {
+      System.out.println("Usage: java NameArray <search_name>");
+      return;
+    }
+
+    String search = args[0];
     boolean result = false;
-    
-    for (int i = 0; i< names.length; i++){
-      if (search.equals(names[i])){
+
+    for (int i = 0; i < names.length; i++) {
+      if (search.equals(names[i])) {
         result = true;
+        break;
       }
-      if ()
+    }
+
+    if (result) {
+      System.out.println(search + "さんは含まれています");
+    } else {
+      System.out.println(search + "さんは含まれていません");
     }
   }
 }
